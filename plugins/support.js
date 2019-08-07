eb1e71ce78fe2686515131581e10c20894756b4d
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-08-07 16:35:43 +0530

    Decorators for DB ops

--- plugins/support.js
+++ plugins/support.js
@@ -6,16 +6,66 @@ const fp = require('fastify-plugin')
 // to export the decorators to the outer scope
 
 module.exports = fp(function (fastify, opts, next) {
-  fastify.decorate('someSupport', function () {
-    return 'hugs'
+ 
+  /*
+   * Create Book with Author Ids and Genere Ids
+   * @param { Object } bookData Instance & metadata of Book
+   * @param { Array } genereIds  array of genere Ids
+   * @param { Array } authorIds  Array of Author Ids
+   */
+  fastify.decorate('createBooks', async (bookData,genereIds,authorIds)=> {
+    const genereArr = []
+    const authorArr =[]
+    const book_doc= await fastify.sql.models.Book.create({
+      ...bookData
+    });
+    
+    if(genereIds && genereIds.length){
+      genereIds.forEach(element => {
+        genereArr.push({genere_id:element,book_id:book_doc.id})
+      });
+    }
+    if(authorIds && authorIds.length){
+      authorIds.forEach(elem=>{
+        authorArr.push({
+          author_id:elem,
+          book_id:book_doc.id
+        });
+      })
+    }
+    if(genereArr && genereArr.length){
+     await fastify.sql.models.bookGenere.bulkCreate(genereArr);
+    }
+  
+    if(authorArr && authorArr.length){
+      await fastify.sql.models.bookAuthor.bulkCreate(authorArr);
+    }
+    return book_doc;
+  });
+
+  /*
+   * Create Generes 
+   * @param { String } name
+   * @param { String } description 
+   * 
+   */
+  fastify.decorate('createGenere', ( name, description)=>{
+    return fastify.sql.models.Genere.create({name,description});
   })
+
+  /*
+   * Create Author 
+   * @param { String } name
+   * @param { String } description 
+   * 
+   */
+  fastify.decorate('createAuthor', ( name, age)=>{
+    return fastify.sql.models.Author.create({name,age});
+  })
+
+
+
   next()
 })
 
-// If you prefer async/await, use the following
-//
-// module.exports = fp(async function (fastify, opts) {
-//   fastify.decorate('someSupport', function () {
-//     return 'hugs'
-//   })
-// })
+
