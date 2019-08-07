19fab93e14c78f1d1faa61ed0e8b3b138dfbf2c8
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-08-07 16:15:45 +0530

    added swagger auto api doc gen

--- app.js
+++ app.js
@@ -16,6 +16,32 @@ module.exports = function (fastify, opts, next) {
     options: Object.assign({}, opts)
   })
 
+  fastify.register(require('fastify-swagger'), {
+    routePrefix: '/documentation',
+    swagger: {
+      info: {
+        title: 'Book CRUD',
+        description: 'Swagger docs',
+        version: '0.1.0'
+      },
+      host: 'localhost',
+      schemes: ['http'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      tags: [
+        { name: 'user', description: 'User related end-points' },
+        { name: 'code', description: 'Code related end-points' }
+      ],
+      securityDefinitions: {
+        apiKey: {
+            type: 'apiKey',
+            name: 'x-session-id',
+            in: 'header',
+        },
+      },
+    },
+exposeRoute:true,
+  })
   // This loads all plugins defined in services
   // define your routes in one of these
   fastify.register(AutoLoad, {
