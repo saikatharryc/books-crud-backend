781aae34eebdfa14628744180b51b2ada5c98bff
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-08-07 18:18:02 +0530

    removing host and schems

--- app.js
+++ app.js
@@ -24,8 +24,6 @@ module.exports = function (fastify, opts, next) {
         description: 'Swagger docs',
         version: '0.1.0'
       },
-      host: 'localhost:3000',
-      schemes: ['http'],
       consumes: ['application/json'],
       produces: ['application/json'],
       tags: [
