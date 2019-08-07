63d2295ad5b2b0d431556710ed12c07dabd53c6d
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2019-08-07 18:18:47 +0530

    genere routes added

---
+++ services/genere/index.js
@@ -0,0 +1,36 @@
+module.exports = function (fastify, opts, next) {
+    
+    fastify.route({
+        url: '/genere/create',
+        method: 'POST',
+        schema: {
+            body: {
+                type: 'object',
+                properties: {
+                    'name': {
+                        type: 'string'
+                      
+                    },
+                    'description': {
+                        type: 'string'
+                    },
+                },
+                required:['name','description']
+            }
+        },
+        // preHandler : [fastify.verifySessionId],
+        handler: async function (request, reply) {
+           
+            fastify.createGenere(request.body.name, request.body.description).then(d=>{
+                return reply.send(d);
+            }).catch(ex=>{
+                fastify.log.error(ex);
+                return reply.status(500).send({
+                 message:"unknown error occured"   
+                })
+            })
+        },
+    })
+
+next()
+}
